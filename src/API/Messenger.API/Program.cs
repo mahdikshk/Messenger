26d487679bab3693eab3cc4d066a1eb63c0fc8b4
Mahdi Kashkalani <mahdiksk88@gmail.com>
2025-02-19 12:22:24 +0330

    Add websocket support to the middleware pipeline

--- src/API/Messenger.API/Program.cs
+++ src/API/Messenger.API/Program.cs
@@ -6,7 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddDataProtection();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,6 +18,12 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+var webSocketOptions = new WebSocketOptions
+{
+    KeepAliveInterval = TimeSpan.FromMinutes(2)
+};
+
+app.UseWebSockets(webSocketOptions);
 app.MapControllers();
 
 app.Run();
