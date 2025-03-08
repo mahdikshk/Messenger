c405192402ac7a21d50c3df6449c523df346c81a
Mahdi Kashkalani <mahdiksk88@gmail.com>
2025-03-08 13:01:13 +0330

    Basic models for login and register related stuff

---
+++ src/Core/Messenger.Application/Models/Identity/LoginResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messenger.Application.Models.Identity
+{
+    public class LoginResponse : IdentityBaseResponse
+    {
+        public string Token { get; set; } = null!;
+    }
+}
---
+++ src/Core/Messenger.Application/Models/Identity/RegisterRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messenger.Application.Models.Identity
+{
+    public class RegisterRequest
+    {
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
