c22c71446941d9384dabb51efed8fedbde1260b2
Mahdi Kashkalani <mahdiksk88@gmail.com>
2025-03-08 13:00:17 +0330

    Base response for Identity stuff

---
+++ src/Core/Messenger.Application/Models/Identity/IdentityBaseResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messenger.Application.Models.Identity
+{
+    public abstract class IdentityBaseResponse
+    {
+        public List<string> Errors { get; set; } = [];
+    }
+}
