907021c4d7e5c87e06ca8f163ab6bf752bf57bfc
Mahdi Kashkalani <mahdiksk88@gmail.com>
2025-03-15 20:33:20 +0330

    Added MediatR and registered it to DI container

---
+++ src/Core/Messenger.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Messenger.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection RegisterApplicationServices(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
+            return services;
+        }
+    }
+}
